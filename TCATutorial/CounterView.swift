88cd36071c2fb242281b07c285dd7908a353c728
Greed <suby0475@gmail.com>
2024-07-23 22:52:08 +0900

    네트워크 통신 추가

--- TCATutorial/CounterView.swift
+++ TCATutorial/CounterView.swift
@@ -13,11 +13,15 @@ struct CounterFeature {
     @ObservableState
     struct State {
         var count = 0
+        var fact: String?
+        var isLoading = false
     }
     
     enum Action {
         case decrementButtonTapped
         case incrementButtonTapped
+        case factButtonTapped
+        case factResponse(String)
     }
     
     var body: some ReducerOf<Self> {
@@ -25,10 +29,26 @@ struct CounterFeature {
             switch action {
             case .decrementButtonTapped:
                 state.count -= 1
+                state.fact = nil
                 return .none
             case .incrementButtonTapped:
                 state.count += 1
+                state.fact = nil
                 return .none
+            case .factButtonTapped:
+                state.fact = nil
+                state.isLoading = true
+                
+                return .run { [count = state.count] send in
+                    let (data, _) = try await URLSession.shared
+                      .data(from: URL(string: "http://numbersapi.com/\(count)")!)
+                    let fact = String(decoding: data, as: UTF8.self)
+                    await send(.factResponse(fact))
+                }
+            case let .factResponse(fact):
+              state.fact = fact
+              state.isLoading = false
+              return .none
             }
         }
     }
@@ -61,6 +81,22 @@ struct CounterView: View {
                 .padding()
                 .background(Color.black.opacity(0.1))
                 .cornerRadius(10)
+            }
+            Button("Fact") {
+                store.send(.factButtonTapped)
+            }
+            .font(.largeTitle)
+            .padding()
+            .background(Color.black.opacity(0.1))
+            .cornerRadius(10)
+            
+            if store.isLoading {
+                ProgressView()
+            } else if let fact = store.fact {
+                Text(fact)
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.center)
+                    .padding()
             }
         }
     }
